93aa3682073db9f5d8f578a1d0b3ea509f1ef6c9
Rafale25 <47261384+Rafale25@users.noreply.github.com>
2024-05-01 04:14:33 +0200

    update

--- src/routes/screenshots/+page.server.js
+++ src/routes/screenshots/+page.server.js
@@ -1,4 +1,5 @@
 import { PRIVATE_SUPABASE_URL, PRIVATE_SUPABASE_SERVICE_KEY } from '$env/static/private'
+// import { env } from '$env/dynamic/private'
 import { createClient } from '@supabase/supabase-js'
 
 const USE_DIRECTLY_FROM_STORAGE = true
