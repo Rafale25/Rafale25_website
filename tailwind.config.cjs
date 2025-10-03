9c4fd5d0d7939fcb1b1cd74ffd1428eb840c39a4
Rafale25 <rafale25.dev@gmail.com>
2025-10-03 12:12:20 +0200

    saturation as css variable

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,13 +4,13 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'primary1': 'hsl(var(--customHue), 20%, 85%)',
-                'primary2': 'hsl(var(--customHue), 20%, 70%)',
-                'primary3': 'hsl(var(--customHue), 20%, 10%)',
-                'primary3lighter': 'hsl(var(--customHue), 20%, 20%)',
-                'primary3light': 'hsl(var(--customHue), 20%, 14%)',
-                'primary3dark': 'hsl(var(--customHue), 20%, 5%)',
-                'primaryBorder': 'hsl(var(--customHue), 20%, 25%)',
+                'primary1':         'hsl(var(--customHue), var(--customSaturation), 85%)',
+                'primary2':         'hsl(var(--customHue), var(--customSaturation), 70%)',
+                'primary3':         'hsl(var(--customHue), var(--customSaturation), 10%)',
+                'primary3lighter':  'hsl(var(--customHue), var(--customSaturation), 20%)',
+                'primary3light':    'hsl(var(--customHue), var(--customSaturation), 14%)',
+                'primary3dark':     'hsl(var(--customHue), var(--customSaturation),  5%)',
+                'primaryBorder':    'hsl(var(--customHue), var(--customSaturation), 25%)',
             },
             screens: {
                 '3xl': '3000px'
