3bdd9817cd4d3bfaeaa8fd7a2e4a78d48f236726
Rafale25 <rafale25.dev@gmail.com>
2025-10-07 18:25:42 +0200

    GP_webring component

---
+++ src/lib/components/GP_webring.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let fontSize = 16;
+    const name = 'rafale25'
+</script>
+
+<div class="flex flex-row gap-1 justify-center items-center" style="font-size: {fontSize}px;">
+    <a target="_self" href="https://graphics-programming.org/webring/frogs/{name}/prev">⬅️</a>
+    <a target="_self" href="https://graphics-programming.org/webring/">
+        <img
+            class="object-contain h-[2.5ex]"
+            src="https://graphics-programming.org/img/froge.webp"
+            alt="a friendly froge"
+        />
+    </a>
+    <a target="_self" href="https://graphics-programming.org/webring/frogs/{name}/next">➡️</a>
+</div>
