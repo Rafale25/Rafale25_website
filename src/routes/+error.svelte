2ac0c8a0fe681fce5f832505fb3c8047ab8db2b6
Rafale25 <rafale25.dev@gmail.com>
2026-05-04 06:31:02 +0200

    Fix non absolute image path

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,11 @@
 <script>
     import { page } from '$app/stores';
+    import { asset } from '$app/paths';
 </script>
 
 <main class="flex w-screen h-screen bg-black overflow-hidden">
     <div class="flex flex-col w-full h-full justify-center items-center gap-2">
-        <img class="w-12 h-auto" src="waaa.png" alt="waaa"/>
+        <img class="w-12 h-auto" src="/waaa.png" alt="waaa"/>
         <h1 class="text-white">{$page.status} {$page.error.message}</h1>
     </div>
 </main>
